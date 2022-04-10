57b25bb35ba8830ceceebb15295525814d97067f
unknown <sourcers99@protonmail.com>
2022-04-10 16:18:27 +0530

    fixed small bug in models

--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.forms import CharField
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import FileExtensionValidator
 
 from config import readConfig
