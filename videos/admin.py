d57718d6f86c718637a4068abf7f80a35791c6a1
unknown <sourcers99@protonmail.com>
2022-04-10 16:48:41 +0530

    Finalize models

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Star, Staralias, Channel, Subsite, Tag, Category, Video
 # Register your models here.
+admin.site.register(Star)
+admin.site.register(Staralias)
+admin.site.register(Channel)
+admin.site.register(Subsite)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Video)
--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.forms import CharField
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from django.core.validators import FileExtensionValidator
 
 from config import readConfig
@@ -29,13 +28,13 @@ class Star(models.Model):
         ("Male", "Male"),
         ("Trans", "Trans")
     ]
-    star_name = models.CharField(_(""), max_length=50)
-    star_image = models.ImageField(_(""), upload_to=data["images-store-path"])
-    star_alias = models.ManyToManyField("Staralias", verbose_name=_(""))
-    star_dob = models.CharField(_(""), max_length=50)
-    star_bio = models.TextField(_(""))
-    star_boob_size = models.CharField(_(""), max_length=50)
-    star_gender = models.CharField(_(""), max_length=50, choices=GENDER, default="Female")
+    star_name = models.CharField(_("Star Name"), max_length=50)
+    star_image = models.ImageField(_("Star Image"), upload_to=data["images-store-path"])
+    star_alias = models.ManyToManyField("Staralias", verbose_name=_("Alias"))
+    star_dob = models.CharField(_("Star DOB"), max_length=50)
+    star_bio = models.TextField(_("Star Bio"))
+    star_boob_size = models.CharField(_("Star Boob Size"), max_length=50)
+    star_gender = models.CharField(_("Star Gender"), max_length=50, choices=GENDER, default="Female")
 
     class Meta:
         verbose_name = _("Star")
@@ -48,8 +47,8 @@ class Star(models.Model):
         return reverse("Star_detail", kwargs={"pk": self.pk})
 
 class Category(models.Model):
-    category_name = models.CharField(_(""), max_length=50)
-    category_image = models.ImageField(_(""), upload_to=data["images-store-path"])
+    category_name = models.CharField(_("Category Name"), max_length=50)
+    category_image = models.ImageField(_("Category Image"), upload_to=data["images-store-path"])
     
 
     class Meta:
@@ -64,8 +63,8 @@ class Category(models.Model):
 
 class Tag(models.Model):
 
-    tag_name = models.CharField(_(""), max_length=50)
-    tag_image = models.ImageField(_(""), upload_to=data["images-store-path"])
+    tag_name = models.CharField(_("Tag Name"), max_length=50)
+    tag_image = models.ImageField(_("Tag Image"), upload_to=data["images-store-path"])
 
     class Meta:
         verbose_name = _("Tag")
@@ -79,8 +78,9 @@ class Tag(models.Model):
 
 class Subsite(models.Model):
 
-    subsite_name = models.CharField(_(""), max_length=50)
-    subsite_image = models.ImageField(_(""), upload_to=data["images-store-path"])    
+    subsite_name = models.CharField(_("Subsite Name"), max_length=50)
+    subsite_image = models.ImageField(_("Subsite Image"), upload_to=data["images-store-path"])
+    subsite_url = models.URLField(_("Subsite URL"), max_length=200)    
 
     class Meta:
         verbose_name = _("subsite")
@@ -95,9 +95,10 @@ class Subsite(models.Model):
 
 class Channel(models.Model):
 
-    channel_name = models.CharField(_(""), max_length=50)
-    channel_image = models.ImageField(_(""), upload_to=data["images-store-path"])
-    channel_subsite = models.ManyToManyField("Subsite", verbose_name=_(""))
+    channel_name = models.CharField(_("Channel Name"), max_length=50)
+    channel_image = models.ImageField(_("Channel Image"), upload_to=data["images-store-path"])
+    channel_subsite = models.ManyToManyField("Subsite", verbose_name=_("Subsites"))
+    channel_url = models.URLField(_("Channel URL"), max_length=200)
 
     class Meta:
         verbose_name = _("Channel")
@@ -111,14 +112,16 @@ class Channel(models.Model):
 
 class Video(models.Model):
 
-    video_name = models.CharField(_(""), max_length=50)
-    video_image = models.ImageField(_(""), upload_to=data["images-store-path"])
-    video_pub_date = models.DateField(_(""), auto_now=False, auto_now_add=False)
-    video_stars = models.ManyToManyField("Star", verbose_name=_(""))
-    video_channel = models.ForeignKey("Channel", verbose_name=_(""), on_delete=models.CASCADE)
-    video_path = models.CharField(_(""), max_length=50)
-    video_tags = models.ManyToManyField("Tag", verbose_name=_(""))
-    video_category = models.ManyToManyField("Category", verbose_name=_(""))
+    video_name = models.CharField(_("Video Name"), max_length=50)
+    video_image = models.ImageField(_("Video Image"), upload_to=data["images-store-path"])
+    video_pub_date = models.DateField(_("Video Published"), auto_now=False, auto_now_add=False)
+    video_desc = models.TextField(_("Video Description"))
+    video_stars = models.ManyToManyField("Star", verbose_name=_("Stars"))
+    video_channel = models.ForeignKey("Channel", verbose_name=_("Channel"), on_delete=models.CASCADE)
+    video_path = models.CharField(_("Video Path"), max_length=50)
+    video_tags = models.ManyToManyField("Tag", verbose_name=_("Video Tag"))
+    video_category = models.ManyToManyField("Category", verbose_name=_("Video Category"))
+    video_url = models.URLField(_("Video URL"), max_length=200)
 
     class Meta:
         verbose_name = _("Video")
